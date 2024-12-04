19584f060111278c2d9da59798cb4bc5f78d5c2b
Cruz Wootten <78571191+LilWikipedia@users.noreply.github.com>
2024-12-04 23:14:55 +0000

    pages

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/my-page">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/More" element={<More />} />
